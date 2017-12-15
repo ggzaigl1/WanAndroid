688f327053147f52eeafc664f293eeebd6f19eff
gab <552040177@qq.com>
2017-12-15 15:28:12 +0800

    新增 BottomNavigationBar

--- app/src/main/java/com/example/gab/babylove/MainActivity.java
+++ app/src/main/java/com/example/gab/babylove/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.gab.babylove;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-import com.example.gab.babylove.activity.DialActivity;
-import com.example.gab.babylove.activity.PhotoViewActivity;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.example.gab.babylove.fragment.FindFragment;
+import com.example.gab.babylove.fragment.NewsMainFragment;
+import com.example.gab.babylove.fragment.VideoHomeFragment;
+import com.example.gab.babylove.fragment.ZhihuDailyFragment;
+import com.example.gab.babylove.utils.StateBarTranslucentUtils;
+import com.example.gab.babylove.utils.StatusBarCompat;
 import com.fy.baselibrary.base.BaseActivity;
-import com.fy.baselibrary.utils.JumpUtils;
+import com.fy.baselibrary.utils.T;
 
 import butterknife.BindView;
 
 /**
  *  主方法
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements BottomNavigationBar.OnTabSelectedListener ,
+                 NavigationView.OnNavigationItemSelectedListener{
 
-    @BindView(R.id.bt_star)
-    Button bt_star;
-    @BindView(R.id.bt_phontview)
-    Button bt_phontview;
+    private FragmentManager mFragmentManager;
+    private ZhihuDailyFragment mZhihuFragment;
+    private VideoHomeFragment mBubbleFragment;
+    private NewsMainFragment mNewsFragment;
+    private FindFragment mFindFragment;
+    private Fragment mCurrentFrag; //当前的fragment
+    @BindView(R.id.fl_content)
+    FrameLayout mFlContent;
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationBar mBottomNavigation;
+    @BindView(R.id.navigation)
+    NavigationView mNavigation;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawer;
 
     @Override
     protected int getContentView() {
@@ -27,7 +51,120 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void init(Bundle savedInstanceState) {
-        bt_star.setOnClickListener(v -> JumpUtils.jump(mContext, DialActivity.class,null));
-        bt_phontview.setOnClickListener(v -> JumpUtils.jump(mContext,PhotoViewActivity.class,null));
+        StateBarTranslucentUtils.setStateBarTranslucent(this);
+        StatusBarCompat.compat(this);
+        mFragmentManager = getSupportFragmentManager();
+        //初始化 主要的fragment 的
+        mZhihuFragment = new ZhihuDailyFragment();
+        mBubbleFragment = new VideoHomeFragment();
+        mNewsFragment = new NewsMainFragment();
+        mFindFragment = new FindFragment();
+        initBottomNavigation();
+        switchContent(mZhihuFragment);
+        mNavigation.setNavigationItemSelectedListener(this);//NavigationView 设置条目点击事前
+//        initNavigation();
+    }
+
+    private void initBottomNavigation() {
+        //设置导航栏背景模式 setBackgroundStyle（）
+        //设置BottomNavigationItem颜色 setActiveColor, setInActiveColor, setBarBackgroundColor
+        /**
+         * 组合
+         * MODE_FIXED+BACKGROUND_STYLE_STATIC 效果
+         * MODE_FIXED+BACKGROUND_STYLE_RIPPLE 效果
+         * MODE_SHIFTING+BACKGROUND_STYLE_STATIC 效果
+         * MODE_SHIFTING+BACKGROUND_STYLE_RIPPLE 效果
+         */
+        mBottomNavigation.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC)
+                .setMode(BottomNavigationBar.MODE_FIXED)
+                .addItem(new BottomNavigationItem(R.mipmap.ic_home, getString(R.string.nav_00_title)).setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_view_headline, getString(R.string.nav_01_title)).setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_live_tv, getString(R.string.nav_02_title)).setActiveColorResource(R.color.colorPrimary))
+                .addItem(new BottomNavigationItem(R.mipmap.icon_find, getString(R.string.find)).setActiveColorResource(R.color.colorPrimary))
+                .setFirstSelectedPosition(0)
+                .setTabSelectedListener(this)
+                .initialise();
+    }
+
+    /**
+     * 动态添加fragment，不会重复创建fragment
+     *
+     * @param to 将要加载的fragment
+     */
+    private void switchContent(Fragment to) {
+        if (mCurrentFrag != to) {
+            if (!to.isAdded()) {
+                if (mCurrentFrag != null) {
+                    mFragmentManager.beginTransaction().hide(mCurrentFrag).commit();
+                }
+                mFragmentManager.beginTransaction().add(R.id.fl_content, to).commit();
+            } else {
+                mFragmentManager.beginTransaction().hide(mCurrentFrag).show(to).commit();
+            }
+            mCurrentFrag = to;
+        }
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        switch (position) {
+            case 0:
+                switchContent(mZhihuFragment);
+                break;
+            case 1:
+                switchContent(mNewsFragment);
+                break;
+            case 2:
+                switchContent(mBubbleFragment);
+                break;
+            case 3:
+                switchContent(mFindFragment);
+                break;
+        }
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            T.showShort("nav_camera");
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            T.showShort("nav_gallery");
+        } else if (id == R.id.nav_slideshow) {
+            T.showShort("nav_slideshow");
+        } else if (id == R.id.nav_manage) {
+            T.showShort("nav_manage");
+        } else if (id == R.id.nav_share) {
+            T.showShort("nav_share");
+        } else if (id == R.id.nav_send) {
+            T.showShort("nav_send");
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/gab/babylove/fragment/FindFragment.java
@@ -0,0 +1,15 @@
+package com.example.gab.babylove.fragment;
+
+import com.example.gab.babylove.R;
+import com.fy.baselibrary.base.BaseFragment;
+
+/**
+ * Created by Gab on 2017/12/15 0015.
+ */
+
+public class FindFragment extends BaseFragment {
+    @Override
+    protected int getContentLayout() {
+        return R.layout.fragment_home;
+    }
+}
