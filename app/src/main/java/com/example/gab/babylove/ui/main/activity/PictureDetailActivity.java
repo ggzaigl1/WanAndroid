88336f9fa46d27f48b3d9b4eaa0c138cba7ab9fd
gab <552040177@qq.com>
2018-05-24 09:02:49 +0800

    修改一些文件

--- app/src/main/java/com/example/gab/babylove/ui/main/activity/PictureDetailActivity.java
+++ app/src/main/java/com/example/gab/babylove/ui/main/activity/PictureDetailActivity.java
@@ -1,36 +1,55 @@
 package com.example.gab.babylove.ui.main.activity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.gab.babylove.R;
 import com.example.gab.babylove.ui.main.adapter.PhotoAdapter;
 import com.example.gab.babylove.entity.OrListBean;
 import com.example.gab.babylove.widget.PhotoViewPager;
+import com.fy.baselibrary.application.IBaseActivity;
 import com.fy.baselibrary.statusbar.MdStatusBar;
 import com.fy.baselibrary.utils.ToastUtils;
+
+import butterknife.BindView;
 
 /**
  * Created by 初夏小溪 on 2018/4/16 0016.
  * 描述：图片详情页
  */
 
-public class PictureDetailActivity extends AppCompatActivity {
+public class PictureDetailActivity extends AppCompatActivity implements IBaseActivity{
 
     private int mCurrentPosition;
+    @BindView(R.id.view_page)
+    PhotoViewPager viewPager;
+    @BindView(R.id.tv_image_count)
+    TextView tv_image_count;
+
+    @Override
+    public boolean isShowHeadView() {
+        return false;
+    }
+
+    @Override
+    public int setView() {
+        return R.layout.activity_picture_detail;
+    }
+
+    @Override
+    public void setStatusBar(Activity activity) {
+        MdStatusBar.setColorBar(this, R.color.black, R.color.black);
+    }
+
     @SuppressLint("SetTextI18n")
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        MdStatusBar.setColorBar(this, R.color.black, R.color.black);
-        setContentView(R.layout.activity_picture_detail);
-        PhotoViewPager viewPager = findViewById(R.id.view_page);
-        TextView tv_image_count = findViewById(R.id.tv_image_count);
-
+    public void initData(Activity activity, Bundle savedInstanceState) {
         OrListBean actionsBean = (OrListBean) getIntent().getExtras().getSerializable("orListBean");
         mCurrentPosition = getIntent().getExtras().getInt("position");
 
@@ -50,5 +69,15 @@ public class PictureDetailActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void reTry() {
+
     }
 }
